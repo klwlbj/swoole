ac11e6bda6e4ee6cf3d96e7e4a161662ee8d537c
klwlbj <1324281299@qq.com>
2020-04-28 17:39:14 +0800

    2.1

--- server/tcp.php
+++ server/tcp.php
@@ -2,16 +2,17 @@
 //创建Server对象，监听 127.0.0.1:9501端口
 $serv = new swoole_server("127.0.0.1", 9501, SWOOLE_PROCESS, SWOOLE_SOCK_TCP);
 
-$serv->set([
-    'worker_num' => 4,//worker进程数 cpu核数1-4倍
-    'max_request' => 10000,
-    'reactor_num' => 2,
-    'open_cpu_affinity' => 1,
-    'log_file' => '/data/log/swoole.log'
-]);
-//$fd客户端连接的唯一标示
+$serv->set(array(
+        'worker_num' => 12,//worker进程数 cpu核数1-4倍
+        'max_request' => 10000,//最大任务数
+        'reactor_num' => 2,
+        'open_cpu_affinity' => 1,
+        // 'log_file' => '/data/log/swoole.log'
+    )
+);
+//$fd，客户端连接的唯一标示,自增，有最大值
 //监听连接进入事件
-//$reactor_id线程id
+//$reactor_id，线程id
 $serv->on('connect', function ($serv, $fd, $reactor_id) {
     echo "Client: {$reactor_id}-{$fd}-Connect.\n";
 });
